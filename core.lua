b07f491efa3f9db88332e73fe54abb1086bda243
D4KiR <d4kir92@gmail.com>
2022-08-23 15:39:44 +0200

    1.0.0-release

--- core.lua
+++ core.lua
@@ -2,7 +2,7 @@
 
 local LibDD = LibStub:GetLibrary("LibUIDropDownMenu-4.0")
 
-MAIVERSION = "2022.08.23-15:10"
+MAIVERSION = "1.0.0"
 
 FLATBORDER = 0.068
 local MAICOLORBACKGROUNDHEADER = 	{0.2, 0.2, 0.2, 0.7}
